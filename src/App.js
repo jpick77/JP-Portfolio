a6578574a94c4bb6aa63cdf528db6e0a2665543d
Jacob Pickron <jpickron30@gmail.com>
2023-04-03 16:34:59 -0600

    .....

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import About from './components/About';
