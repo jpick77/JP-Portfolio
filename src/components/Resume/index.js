0952a46cde021e2be2cb4e97fd18186b7176359e
Jacob Pickron <jpickron30@gmail.com>
2023-04-03 19:24:35 -0600

    made the resume downloadable

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,6 @@
 import './index.scss'
 import ResumePic from '../images/resume.png'
+import ResumeDoc from '../Resume/resume.pdf'
 import Footer from '../Footer'
 import Header from '../Header'
 
@@ -11,7 +12,7 @@ const Resume = () => {
 
         <h1 className='resume-link'>Resume</h1>
         <div className='resume-pics'>
-        <a href="Jacob Pickron Resume Dec 2019.docx" target='blank' >
+        <a href={ResumeDoc} target='blank' >
                 {/* Algorhythyms */}
                 <img className="pic-resume" src={ResumePic} alt=''></img>
                 </a>
